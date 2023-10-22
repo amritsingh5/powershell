1fa409ada7721a41333e2279589e66695182cf09
amritsingh5 <146261015+amritsingh5@users.noreply.github.com>
2023-10-22 17:43:43 -0400

    Add files via upload

---
+++ cmdlets.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+	Lists all PowerShell cmdlets
+.DESCRIPTION
+	This PowerShell script lists all PowerShell cmdlets.
+#>
+
+try {
+	Get-Command -Command-Type cmdlet
+	exit 0 # success
+} catch {
+	"Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
